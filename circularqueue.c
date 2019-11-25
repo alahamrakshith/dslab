49a5262e465404e4a63032d17249a7f76bc6e2b6
Rakshith A G <44615912+alahamrakshith@users.noreply.github.com>
2019-11-25 23:18:17 +0530

    clean

--- circularqueue.c
+++ circularqueue.c
@@ -1,121 +1,68 @@
-# include<stdio.h>
+#include <stdio.h>
+int queue[50];
+int front=-1,rear=-1,size=50;
 
+void insert(int ele)
+{
 
-int cqueue_arr[5];
-int front = -1;
-int rear = -1;
-
-
-void insert(int item,int max)
-{
-if((front == 0 && rear == max-1) || (front == rear+1))
-{
-printf("Queue Overflow \n");
-return;
-}
-if (front == -1)
-{
-front = 0;
-rear = 0;
-}
-else
-{
-if(rear == max-1)
-rear = 0;
-else
-rear = rear+1;
-}
-cqueue_arr[rear] = item ;
-}
-void del(int max)
-{
-if (front == -1)
-{
-printf("Queue Underflow\n");
-return ;
-}
-printf("Element deleted from queue is : %d\n",cqueue_arr[front]);
-if(front == rear)
-{
-front = -1;
-rear=-1;
-}
-else
-{
-if(front == max-1)
-front = 0;
-else
-front = front+1;
-}
+    if((rear+1)%size==front)
+        printf("Overflow\n");
+    else if(rear==-1)
+    {
+        front=rear=0;
+        queue[rear]=ele;
+    }
+    else
+    {
+        rear=(rear+1)%size;
+        queue[rear]=ele;
+    }
 }
 
-void display(int max)
+int del()
 {
-int front_pos = front,rear_pos = rear;
-if(front == -1)
-{
-printf("Queue is empty\n");
-return;
+    if(front==-1)
+        printf("Underflow\n");
+    else if(front==rear)
+    {
+        printf("%d",queue[front]);
+        front=rear=-1;
+    }
+    else
+    {
+        printf("%d",queue[front]);
+        front=(front+1)%size;
+    }
 }
-printf("Queue elements :\n");
-if( front_pos <= rear_pos )
-while(front_pos <= rear_pos)
-{
-printf("%d ",cqueue_arr[front_pos]);
-front_pos++;
-}
-else
-{
-while(front_pos <= max-1)
-{
-printf("%d ",cqueue_arr[front_pos]);
-front_pos++;
-}
-front_pos = 0;
-while(front_pos <= rear_pos)
-{
-printf("%d ",cqueue_arr[front_pos]);
-front_pos++;
-}
-}
-printf("\n");
+
+void print()
+{   int i;
+    if(front!=-1)
+    {
+        for(i=front;i!=rear;i=(i+1)%size)
+            printf("%d",queue[i]);
+        printf("%d",queue[i]);
+    }
+    else
+        printf("No elements in array\n");
 }
 
 int main()
-{
-
-
-int choice,item;
-do
-{
-printf("1.Insert\n");
-printf("2.Delete\n");
-printf("3.Display\n");
-printf("4.Quit\n");
-
-printf("Enter your choice : ");
-scanf("%d",&choice);
-
-switch(choice)
-{
-case 1 :
-printf("Input the element for insertion in queue : ");
-scanf("%d", &item);
-
-insert(item,5);
-break;
-case 2 :
-del(5);
-break;
-case 3:
-display(5);
-break;
-case 4:
-break;
-default:
-printf("Wrong choice\n");
-}
-}while(choice!=4);
-
-return 0;
+{   int ch,num;
+    while(1)
+    {
+        printf("1.insert\n2.del\n3.print\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: printf("Enter element\n");
+                    scanf("%d",&num);
+                    insert(num);
+                    break;
+            case 2: del();
+                    break;
+            case 3: print();
+                    break;
+        }
+    }
 }
